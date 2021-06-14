de3884df48b1452ccc8d3082379b9bdc277baccc
Gregory Casamento <greg.casamento@gmail.com>
2021-06-14 14:38:35 -0400

    Initial Commit

---
+++ NSCollectionViewLayout_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSCollectionViewLayout_test
+//
+//  Created by Gregory Casamento on 6/14/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
