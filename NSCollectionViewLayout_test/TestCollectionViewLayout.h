2b065ffb2a063a5a596bb8841bcce19419415ed3
Gregory John Casamento <greg.casamento@gmail.com>
2022-10-05 04:31:56 -0400

    Add test layout

---
+++ NSCollectionViewLayout_test/TestCollectionViewLayout.h
@@ -0,0 +1,16 @@
+//
+//  TestCollectionViewLayout.h
+//  NSCollectionViewLayout_test
+//
+//  Created by Gregory Casamento on 10/5/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestCollectionViewLayout : NSCollectionViewLayout
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NSCollectionViewLayout_test/TestCollectionViewLayout.m
@@ -0,0 +1,12 @@
+//
+//  TestCollectionViewLayout.m
+//  NSCollectionViewLayout_test
+//
+//  Created by Gregory Casamento on 10/5/22.
+//
+
+#import "TestCollectionViewLayout.h"
+
+@implementation TestCollectionViewLayout
+
+@end
